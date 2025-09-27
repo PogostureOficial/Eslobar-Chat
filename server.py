0e8c27700fc122f5b9d666858b7497f8a70ab3b1
PogostureOficial <pogostureoficial@gmail.com>
2025-09-26 21:25:05 -0300

    Update server.py

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ import openai
 import traceback
 import os
 
-app = Flask(__name__, static_folder='static')
+app = Flask(__name__)
 
 CORS(app)
 
@@ -62,4 +62,5 @@ def ask():
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 10000))
 app.run(host='0.0.0.0', port=port)
+
 
