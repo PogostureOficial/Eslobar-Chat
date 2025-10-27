bfacc34e2876bcc4da2ccf6824a0ca9482d16391
PogostureOficial <pogostureoficial@gmail.com>
2025-10-27 16:29:32 -0300

    Update server.py

--- server.py
+++ server.py
@@ -324,8 +324,10 @@ def ask():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port, debug=False)
+
+
 
 
 
